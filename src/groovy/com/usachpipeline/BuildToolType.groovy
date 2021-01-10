7ef8008f520922d10af991a862f02258230b3400
Nicolas Hermosilla <nhermosilla1987@gmail.com>
2021-01-10 16:51:59 -0300

    add build tool function

--- src/groovy/com/usachpipeline/BuildToolType.groovy
+++
@@ -1,4 +0,0 @@
-enum BuildToolType{
-  Maven,
-  Gradle
-}
--- vars/ejecucion.groovy
+++ vars/ejecucion.groovy
@@ -6,13 +6,6 @@ def call() {
                 steps {
                     script {
                         echo 'INICIO PIPELINE'
-                        def buildtool1 = buildtool.get()
-
-                        if (buildtool1 == 'maven') {
-                            echo 'Buld tool es MAVEN'
-                        }else {
-                            echo 'Buld tool es Gradle'
-                        }
                     }
                 }
             }
