15d170bab17be7084e6d67284fb271d5abab05a5
Nicolas Hermosilla <nhermosilla1987@gmail.com>
2021-01-11 10:02:58 -0300

    fix

--- vars/validate.groovy
+++ vars/validate.groovy
@@ -1,45 +1,36 @@
 
 //Validar el tipo de rama a ejecutar (feature, develop o release)
 //USO : validate.getBranchName()
-
 def getBranchName() {
-
-    def branches = ['develop', 'main'];
+    def branches = ['develop', 'main']
 
     def branch = env.GIT_BRANCH
 
-   if(branches.contains(env.GIT_BRNACH){
-       return env.GIT_BRNACH
-   }
+    if (branches.contains(branch)) {
+        return env.GIT_BRNACH
+    }
 
-    if(true)
-   {
-       return "feature"
-   }
+    if (isFeature(branch)) {
+        return 'feature'
+    }
 
-   if(validateReleaseNameFormat(env.GIT_BRNACH))
-   {
-       return "release"
-   }
-
+    if (isRelease(branch)) {
+        return 'release'
+    }
 }
 
-def isFeatureBranch() {
-
-    return branchName==env.GIT_BRANCH
-
+def isFeature(String branchName) {
+    return branchName =~ /feature*/
 }
 
 //USO : if (validate.isBranchName('develop')) {}
 def isBranchName(String branchName) {
-
-    return branchName==env.GIT_BRANCH
-
+    return branchName == env.GIT_BRANCH
 }
 
 //Validar formato de nombre de rama release según patrón
 //release-v{major}-{minor}-{patch}
 //USO :  if (validate.validateReleaseNameFormat('release-v1-0-0')) {  echo 'OK' }
-def validateReleaseNameFormat(String releaseName) {
-    return releaseName =~ /^(release-v[0-9]+)\-([0-9]+)\-([0-9]+)?$/
+def isRelease(String branchName) {
+    return branchName =~ /^(release-v[0-9]+)\-([0-9]+)\-([0-9]+)?$/
 }
