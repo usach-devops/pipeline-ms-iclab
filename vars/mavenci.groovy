a06c84c8a6b5c88842123a56c89cd53d04578d41
Gerardo Felmer <desarrollo@satelnet.cl>
2021-01-11 17:40:50 -0300

    listo para PR, nexus y sonar funcionando

--- vars/mavenci.groovy
+++ vars/mavenci.groovy
@@ -27,6 +27,7 @@ def execute() {
         env.JENKINS_STAGE = env.STAGE_NAME
         echo env.JENKINS_STAGE
         //imagino que las versiones de esto deben ser progresivas?
+        //se crea en nexus repositorio mavenci-repo
         nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'mavenci-repo', 
         packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'build/DevOpsUsach2020-0.0.1.jar']], 
         mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
