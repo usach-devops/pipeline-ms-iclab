0cf0ef2201aaee33a4b414240c93d7c4687930a3
Gerardo Felmer <desarrollo@satelnet.cl>
2021-01-11 00:15:46 -0300

    se modifica isBranchName y se agrega origina a validacion de nombre de rama release

--- vars/validate.groovy
+++ vars/validate.groovy
@@ -13,14 +13,17 @@ def getBranchName() {
 //USO : if (validate.isBranchName('develop')) {}
 def isBranchName(String branchName) {
 
-    return env.GIT_BRNACH==env.GIT_BRANCH
+    return branchName==env.GIT_BRANCH
 
 
 }
 
 //Validar formato de nombre de rama release según patrón 
 //release-v{major}-{minor}-{patch}
-//USO :  if (validate.validateReleaseNameFormat('release-v1-0-0')) {  echo 'OK' }
+//USO :  if (validate.validateReleaseNameFormat('origin/release-v1-0-0')) {  echo 'OK' }
+//env entrega :
+//GIT_BRANCH=origin/release-v1-1-3
+//GIT_LOCAL_BRANCH=release-v1-1-3
 def validateReleaseNameFormat(String releaseName) {
-    return releaseName =~ /^(release-v[0-9]+)\-([0-9]+)\-([0-9]+)?$/
+    return releaseName =~ /^(origin\/release-v[0-9]+)\-([0-9]+)\-([0-9]+)?$/
 }
