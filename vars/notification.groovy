c492da8a63d926dfaeee7a707a8fbadea6d7830b
Gerardo Felmer <desarrollo@satelnet.cl>
2021-01-11 12:46:44 -0300

    se elimina archivo

--- vars/notification.groovy
+++ vars/notification.groovy
@@ -1,20 +1,19 @@
 //Mensaje por defecto, indicando maven/gradle y el step donde se produjo el error
 //por cada step se tiene que agregar env.JENKINS_STAGE = env.STAGE_NAME
-def sendFailureMsg() {
-    def MSG_BASE = "Build Failure: [devops-usach-grupo2][Proyecto ${env.BUILD_TOOL}] [Stage ${env.JENKINS_STAGE}]";
-    slackSend color: "danger", message: "${MSG_BASE}", teamDomain: 'devops-usach-2020', tokenCredentialId: 'slack-credentials'
+def failure() {
+    def template = "Build Failure: [devops-usach-grupo2][Proyecto ${env.BUILD_TOOL}] [Stage ${env.JENKINS_STAGE}]";
+    slackSend color: "danger", message: "${template}", teamDomain: 'devops-usach-2020', tokenCredentialId: 'slack-credentials'
 }
 
-def sendFailureMsg(String msg) {
+def failure(msg) {
     slackSend color: "danger", message: "Build Failure: ${msg}", teamDomain: 'devops-usach-2020', tokenCredentialId: 'slack-credentials'
 }
 
-//Mensaje por defecto, indicando maven/gradle y el build success
-def sendSuccessMsg() {
-    def MSG_BASE = "Build Success: [devops-usach-grupo2][Proyecto ${env.BUILD_TOOL}]";
-    slackSend color: "good", message: "${MSG_BASE}", teamDomain: 'devops-usach-2020', tokenCredentialId: 'slack-credentials'
+def success() {
+    def template = "Build Success: [devops-usach-grupo2][Proyecto ${env.BUILD_TOOL}]";
+    slackSend color: "good", message: "${template}", teamDomain: 'devops-usach-2020', tokenCredentialId: 'slack-credentials'
 }
 
-def sendSuccessMsg(String msg) {
+def success(msg) {
     slackSend color: "good", message: "Build Success: ${msg}", teamDomain: 'devops-usach-2020', tokenCredentialId: 'slack-credentials'
 }
