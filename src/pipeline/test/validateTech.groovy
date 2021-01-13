d271af2da25b3911f35de0c6237431bcd3e678b7
mcontrerass <mcontrerass@gmail.com>
2021-01-13 11:46:30 -0300

    Validacion tecnologia de desarrollo con src/pipeline/test/validateTech.groovy

---
+++ src/pipeline/test/validateTech.groovy
@@ -0,0 +1,17 @@
+package pipeline.test
+//Validar el tipo de tecnología de la aplicación (ms, front, bff, etc)
+//USO : validateTech.get()
+def get() {
+   def tech = ['ms', 'front', 'bff']
+	def repo = env.GIT_URL //.getUserRemoteConfigs()[0].getUrl().tokenize('/').last().split("\\.")[0]
+	println "Repo: ${repo}"
+
+	for (item in tech) {
+		println "Item: ${item}"
+    		if (repo.contains(item)) {
+			return item
+		}
+	}
+}
+
+return this;
