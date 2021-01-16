435f30e848c73bae64dfaa757bec683f7abf4fd3
Flavio Soto Aburto <67344555+flsotoa@users.noreply.github.com>
2021-01-16 10:37:09 -0300

    Update pipelinecd.groovy
    se modifica nombre stage a nexusDownload

--- vars/pipelinecd.groovy
+++ vars/pipelinecd.groovy
@@ -7,18 +7,18 @@ def execute() {
         echo env.JENKINS_STAGE
     
     }
-	stage('downloadNexus') {
-		env.JENKINS_STAGE = env.STAGE_NAME
-		echo env.JENKINS_STAGE
-		//se descarga repositorio mavenci-repo
-		sh 'curl -X GET -u admin:DevOps2020 http://192.81.214.49:8081/repository/mavenci-repo/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar -O'
+    stage('nexusDownload') {
+	env.JENKINS_STAGE = env.STAGE_NAME
+	echo env.JENKINS_STAGE
+	//se descarga repositorio mavenci-repo
+	sh 'curl -X GET -u admin:DevOps2020 http://192.81.214.49:8081/repository/mavenci-repo/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar -O'
     }
-	
-	stage('run') {
-		//Ejecutar artefacto descargado.
+
+    stage('run') {
+	//Ejecutar artefacto descargado.
         env.JENKINS_STAGE = env.STAGE_NAME
         echo env.JENKINS_STAGE
-		//falta el directorio local del job-nexus  {
+	//falta el directorio local del job-nexus
         //sh 'java -jar DevOpsUsach2020-0.0.1.jar &'
 
     }
