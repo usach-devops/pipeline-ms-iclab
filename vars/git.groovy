aa9405ba3bbcdbecbcfb3e822275e882b3948e11
Gerardo Felmer <desarrollo@satelnet.cl>
2021-01-16 19:01:09 -0300

    se agrega git.merge y las llamadas desde los stages

--- vars/git.groovy
+++ vars/git.groovy
@@ -2,4 +2,12 @@ def diff(){
     figlet 'git diff'
     sh 'git diff origin/main..' + env.GIT_BRANCH
 }
+
+def merge(branchfrom, branchto) {
+    figlet 'git merge'
+    echo 'Merge '+branchfrom+' into '+branchto
+    sh 'git checkout '+branchto
+    sh 'git merge origin/' + branchfrom
+}
+
 return this
--- vars/pipelinecd.groovy
+++ vars/pipelinecd.groovy
@@ -4,46 +4,74 @@ def execute() {
 
     stage('gitDiff') {
         git.diff()
-    
     }
+
     stage('nexusDownload') {
-	env.JENKINS_STAGE = env.STAGE_NAME
-	echo env.JENKINS_STAGE
-	//se descarga repositorio mavenci-repo
-	sh 'curl -X GET -u admin:DevOps2020 http://192.81.214.49:8081/repository/mavenci-repo/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar -O'
+        try {
+            env.JENKINS_STAGE = env.STAGE_NAME
+            echo env.JENKINS_STAGE
+            //se descarga repositorio mavenci-repo
+            sh 'curl -X GET -u admin:DevOps2020 http://192.81.214.49:8081/repository/mavenci-repo/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar -O'
+        }catch (Exception e){
+            executeError(e)
+        }
     }
 
     stage('run') {
-	//Ejecutar artefacto descargado.
-        env.JENKINS_STAGE = env.STAGE_NAME
-        echo env.JENKINS_STAGE
-	//falta el directorio local del job-nexus
-        //sh 'java -jar DevOpsUsach2020-0.0.1.jar &'
-
+        try{
+            //Ejecutar artefacto descargado.
+            env.JENKINS_STAGE = env.STAGE_NAME
+            echo env.JENKINS_STAGE
+            //falta el directorio local del job-nexus
+            //sh 'java -jar DevOpsUsach2020-0.0.1.jar &'
+        }catch (Exception e){
+            executeError(e)
+        }
     }
 	
     stage('Test') {
-        env.JENKINS_STAGE = env.STAGE_NAME
-        echo env.JENKINS_STAGE
-        sh 'curl -X GET http://localhost:8081/rest/mscovid/test?msg=testing'
+        try {
+            env.JENKINS_STAGE = env.STAGE_NAME
+            echo env.JENKINS_STAGE
+            sh 'curl -X GET http://localhost:8081/rest/mscovid/test?msg=testing'
+        }catch (Exception e){
+            executeError(e)
+        }
     }
 	
     stage('gitMergeMaster') {
-        env.JENKINS_STAGE = env.STAGE_NAME
-        echo env.JENKINS_STAGE
-       
+        try {
+            env.JENKINS_STAGE = env.STAGE_NAME
+            echo env.JENKINS_STAGE
+            git.merge(env.GIT_BRANCH,'main')
+        }catch (Exception e){
+            executeError(e)
+        }
     }
 	
 	stage('gitMergeDevelop') {
-        env.JENKINS_STAGE = env.STAGE_NAME
-        echo env.JENKINS_STAGE
-        
+        try {
+            env.JENKINS_STAGE = env.STAGE_NAME
+            echo env.JENKINS_STAGE
+            git.merge(env.GIT_BRANCH,'develop')
+        }catch (Exception e){
+            executeError(e)
+        }
     }
+
 	stage('gitTagMaster') {
         env.JENKINS_STAGE = env.STAGE_NAME
         echo env.JENKINS_STAGE
-        
 	}	
+
+}
+
+def executeError(e) {
+    //error para output del pipeline mas detallado
+    echo "OUTPUT ERROR ${e.toString()}"
+    //Error para slack desde post actions en ejecucion.groovy
+    def message = env.ERROR_MESSAGE == '' || env.ERROR_MESSAGE == null ? "[Stage ${env.JENKINS_STAGE}] Pipeline aborted due stage failure" : env.ERROR_MESSAGE 
+    error message
 
 }
 
