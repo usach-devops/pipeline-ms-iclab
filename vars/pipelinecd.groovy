b90a5cde5b3d2277d353558ed0f607eae365e87a
Flavio Soto Aburto <67344555+flsotoa@users.noreply.github.com>
2021-01-16 17:58:02 -0300

    Update pipelinecd.groovy
    Se agrega stage run y test, se ingresa figlet Continuous Deployment

--- vars/pipelinecd.groovy
+++ vars/pipelinecd.groovy
@@ -1,6 +1,8 @@
 def execute() {
+
     def branchName = validate.getBranchName()
     println 'run maven cd'
+    figlet 'Continuous Deployment'
 
     stage('gitDiff') {
         git.diff()
@@ -19,7 +21,7 @@ def execute() {
         echo env.JENKINS_STAGE
 	//cd '/var/jenkins_home/workspace/ci-cd/cd-pipeline'
         sh 'java -jar DevOpsUsach2020-0.0.1.jar &'
-	sh 'sleep 100'
+	sh 'sleep 20'
 
     }
 	
