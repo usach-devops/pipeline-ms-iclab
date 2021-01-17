7bd1607c3c9dd8e57b25fe66e7c29763f4e810c2
mcontrerass <mcontrerass@gmail.com>
2021-01-16 22:33:50 -0300

    Se corrige error en stage gitCreateRelease en CI

--- src/pipeline/git/GitMethods.groovy
+++ src/pipeline/git/GitMethods.groovy
@@ -24,7 +24,7 @@ def createBranch(String origin, String newBranch) {
         git fetch -p
         git checkout '''+origin+'''; git pull origin '''+origin+''' 
         git checkout -b '''+newBranch+'''
-        git remote set-url origin https://mcontrerass:X1Ex1en!@github.com/usach-devops/pipeline-ms-iclab.git
+        git remote set-url origin https://mcontrerass:X1Ex1en!@github.com/usach-devops/ms-iclab-gradle.git
         git push origin '''+newBranch+'''
         git checkout '''+origin+'''; git pull origin '''+origin+''' 
         git branch -d '''+newBranch+'''
