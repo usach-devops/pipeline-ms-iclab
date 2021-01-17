4e1db0ddacd8617dd3ad8824a21853405ca79cfd
Nicolas Hermosilla <nhermosilla1987@gmail.com>
2021-01-16 23:03:35 -0300

    test

--- vars/git.groovy
+++ vars/git.groovy
@@ -1,7 +1,8 @@
 def diff(){
     figlet 'git diff'
-    sh 'git checkout main; git pull'
-    sh 'git checkout '+ env.GIT_BRANCH +'; git pull'
-    sh 'git diff main..' + env.GIT_BRANCH +' --'
+    sh 'git status'
+    // sh 'git checkout main; git pull'
+    // sh 'git checkout '+ env.GIT_BRANCH +'; git pull'
+    // sh 'git diff main..' + env.GIT_BRANCH +' --'
 }
 return this
