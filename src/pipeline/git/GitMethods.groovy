a56e9786311b74b99e33e5c61c38d36759d32586
Nicolas Hermosilla <nhermosilla1987@gmail.com>
2021-01-17 16:08:22 -0300

    withcredentials

--- src/pipeline/git/GitMethods.groovy
+++ src/pipeline/git/GitMethods.groovy
@@ -1,6 +1,9 @@
 package pipeline.git
 
 def checkIfBranchExists(String branch) {
+    
+    sh 'git branch --merged=main'
+
     def output = sh (script: "git ls-remote --heads origin ${branch}", returnStdout: true)
     echo 'branch existe: ' + output
 
@@ -25,9 +28,9 @@ def createBranch(String origin, String newBranch) {
 
         sh '''
         git remote set-url origin https://${GIT_USER}:${GIT_PASS}@github.com/usach-devops/ms-iclab.git
-        
         git fetch -p
-        git checkout '''+origin+'''; git pull origin '''+origin+''' 
+        git checkout '''+origin+''' 
+        git pull origin '''+origin+''' 
         git checkout -b '''+newBranch+'''
         git push origin '''+newBranch+'''
         git checkout '''+origin+'''; git pull origin '''+origin+''' 
