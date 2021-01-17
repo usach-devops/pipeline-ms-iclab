e68a4b8c26af959082d76557a026cf5ee05b9fc8
Gerardo Felmer <desarrollo@satelnet.cl>
2021-01-17 16:47:47 -0300

    se agrega nombre para el tag en release

--- vars/pipelinecd.groovy
+++ vars/pipelinecd.groovy
@@ -64,7 +64,7 @@ def execute() {
 	stage('gitTagMaster') {
         env.JENKINS_STAGE = env.STAGE_NAME
         echo env.JENKINS_STAGE
-        git.tag('main','feature-v1-1-1')
+        git.tag('main',validate.getValidBranchName())
 	}	
 }
 
