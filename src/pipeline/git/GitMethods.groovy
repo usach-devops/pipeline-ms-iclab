461f4e545843d561dcb0910ce47938fe1122eeb4
mcontrerass <mcontrerass@gmail.com>
2021-01-16 23:33:24 -0300

    Se corrige error en stage gitCreateRelease en CI

--- src/pipeline/git/GitMethods.groovy
+++ src/pipeline/git/GitMethods.groovy
@@ -21,7 +21,7 @@ def createBranch(String origin, String newBranch) {
     echo "newBranch:  ${newBranch}"
     sh '''
         git remote set-url origin https://nicolashermosilla:50ed7c548a7b92631c8aa81577ef02b862e67b5b@github.com/usach-devops/ms-iclab.git
-        git branch -d '''+newBranch+'''
+        
         git fetch -p
         git checkout '''+origin+'''; git pull origin '''+origin+''' 
         git checkout -b '''+newBranch+'''
@@ -29,6 +29,7 @@ def createBranch(String origin, String newBranch) {
         git checkout '''+origin+'''; git pull origin '''+origin+''' 
         git branch -d '''+newBranch+'''
     '''
+    //git branch -d '''+newBranch+'''
 }
 
 return this;
