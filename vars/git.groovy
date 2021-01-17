237a0affc45ee3968e77e601ddf2802ee92d276a
Nicolas Hermosilla <nhermosilla1987@gmail.com>
2021-01-16 23:31:19 -0300

    test

--- vars/git.groovy
+++ vars/git.groovy
@@ -4,6 +4,7 @@ def diff(){
     sh 'git branch'
     sh 'git remote -v'
     sh 'git clean -f'
+    sh 'git checkout main; git pull'
     sh 'git diff origin/main..origin/develop --'
     // sh 'git checkout main; git pull'
     // sh 'git checkout '+ env.GIT_BRANCH +'; git pull'
