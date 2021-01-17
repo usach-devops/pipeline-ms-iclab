3b6d79b09cb51d384693f955cbf12bb3cc155ee8
Nicolas Hermosilla <nhermosilla1987@gmail.com>
2021-01-17 18:40:30 -0300

    message

--- vars/notification.groovy
+++ vars/notification.groovy
@@ -2,13 +2,13 @@
 //Mensaje por defecto, indicando maven/gradle y el step donde se produjo el error
 //por cada step se tiene que agregar env.JENKINS_STAGE = env.STAGE_NAME
 def failure() {
-    def template = "Build Failure: [devops-usach-grupo2][Proyecto ${env.BUILD_TOOL}] [Stage ${env.JENKINS_STAGE}]"
+    def template = "[Grupo2][Pipeline ${env.PIPELINE_TYPE}][Rama: ${validate.getValidBranchName()}][Stage: ${env.JENKINS_STAGE}][Proyecto ${env.BUILD_TOOL}][Resultado: No Ok]"
     slackSend color: 'danger', message: "${template}", teamDomain: 'devops-usach-2020', tokenCredentialId: 'slack-credentials'
 }
 
 def failure(msg) {
-    def template = "Build Failure: [devops-usach-grupo2][Proyecto ${env.BUILD_TOOL}]"
-    slackSend color: 'danger', message: "Build Failure: ${template} ${msg}", teamDomain: 'devops-usach-2020', tokenCredentialId: 'slack-credentials'
+    def template = "[Grupo2][Pipeline ${env.PIPELINE_TYPE}][Rama: ${validate.getValidBranchName()}][Stage: ${env.JENKINS_STAGE}][Proyecto ${env.BUILD_TOOL}][Resultado: No Ok][Detalle: ${msg}]"
+    slackSend color: 'danger', message: template, teamDomain: 'devops-usach-2020', tokenCredentialId: 'slack-credentials'
 }
 
 def success() {
@@ -17,6 +17,6 @@ def success() {
 }
 
 def success(msg) {
-    def template = "[Grupo2][Pipeline ${env.PIPELINE_TYPE}][Rama: ${validate.getValidBranchName()}][Stage: ${env.JENKINS_STAGE}][Proyecto ${env.BUILD_TOOL}][Resultado: Ok]"
+    def template = "[Grupo2][Pipeline ${env.PIPELINE_TYPE}][Rama: ${validate.getValidBranchName()}][Stage: ${env.JENKINS_STAGE}][Proyecto ${env.BUILD_TOOL}][Resultado: Ok][Detalle: ${msg}]"
     slackSend color: 'good', message: template, teamDomain: 'devops-usach-2020', tokenCredentialId: 'slack-credentials'
 }
